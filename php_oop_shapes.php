f3f21aaa0c0d20fab5cdbb035c6f7bc879375032
zekero1 <rjayocon06@gmail.com>
2023-05-29 19:22:26 +0800

    php oop shapes

---
+++ php_oop_shapes.php
@@ -0,0 +1,106 @@
+<?php
+abstract class Shape {
+    protected $name;
+    abstract public function description();
+}
+
+
+interface IShape {
+  public function getArea($length, $width);
+  public function getPerimeter($length, $width);
+}
+
+interface ICircle {
+    public function getAreaOfCircle($radius);
+    public function getPerimeter($radius);
+}
+
+interface ITriangle {
+    public function getPerimeter($side1, $side2, $side3);
+}
+
+class Square extends Shape implements IShape {
+    public function description() {
+        return "Square has four equal sides.";
+    }
+
+    public function getArea($length, $width) {
+        if ($length != $width) {
+            return "Invalid";
+        }
+
+        return $length * $width;
+    }
+
+    public function getPerimeter($length, $width) {
+        if ($length != $width) {
+            return "Invalid";
+        }
+
+        return 4 * $length;
+    }
+}
+
+class Rectangle extends Shape implements IShape {
+    public function description() {
+        return "Rectangle has two equal sides.";
+    }
+
+    public function getArea($length, $width) {
+        return $length * $width;
+    }
+
+    public function getPerimeter($length, $width) {
+        return 2 * ($length + $width);
+    }
+}
+
+class Triangle extends Shape implements ITriangle {
+    public function description() {
+        return "Triangle has three sides.";
+    }
+
+    public function getArea($base, $height) {
+        return ($base * $height) / 2;
+    }
+
+    public function getPerimeter($side1, $side2, $side3) {
+        return $side1 + $side2 + $side3;
+    }
+}
+
+class Circle extends Shape implements ICircle {
+    public function description() {
+        return "Circle has no sides, only a curve.";
+    }
+
+    public function getAreaofCircle($radius) {
+        return round(pi() * pow($radius, 2), 2);
+    }
+
+    public function getPerimeter($radius) {
+        return round(2 * pi() * $radius, 3);
+    }
+}
+
+$shape1 = new Square();
+echo $shape1->description() . "\n"; // output "Square has four equal sides."
+echo $shape1->getArea(4, 4) . "\n"; // output 16
+echo $shape1->getArea(4, 3) . "\n"; // output "Invalid" as length and width need to be equal
+echo $shape1->getPerimeter(4, 4) . "\n"; // output 16
+echo $shape1->getPerimeter(4, 3) . "\n"; // output "Invalid" as length and width need to be equal
+
+$shape2 = new Rectangle();
+echo $shape2->description() . "\n"; // output "Rectangle has two equal sides."
+echo $shape2->getArea(4, 6) . "\n"; // output 24
+echo $shape2->getPerimeter(4, 6) . "\n"; // output 20
+
+$shape3 = new Triangle();
+echo $shape3->description(); // output "Triangle have three sides."
+echo $shape3->getArea(4,6); // output 12
+echo $shape3->getPerimeter(4,6,7); // output 17
+
+$shape4 = new Circle();
+echo $shape4->description(); // output "Circle have no sides only curve."
+echo $shape4->getAreaOfCircle(5); // output 78.54, consider the first parameter as the radius value
+echo $shape4->getPerimeter(5); // output 31.416, consider the first parameter as the radius value
