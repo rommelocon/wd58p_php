40175e0325d662858e614ca3918c1cbc407af257
zekero1 <rjayocon06@gmail.com>
2023-05-29 19:33:22 +0800

    php oop shapes fix

--- php_oop_shapes.php
+++ php_oop_shapes.php
@@ -1,30 +1,31 @@
 <?php
-abstract class Shape {
+abstract class Shape
+{
     protected $name;
     abstract public function description();
 }
 
 
-interface IShape {
-  public function getArea($length, $width);
-  public function getPerimeter($length, $width);
+interface IShape
+{
+    public function getArea($length, $width);
+    public function getPerimeter($length, $width);
 }
 
-interface ICircle {
-    public function getAreaOfCircle($radius);
-    public function getPerimeter($radius);
+interface ITriangle
+{
+    public function getPerimeter($length, $width, $height);
 }
 
-interface ITriangle {
-    public function getPerimeter($side1, $side2, $side3);
-}
-
-class Square extends Shape implements IShape {
-    public function description() {
+class Square extends Shape implements IShape
+{
+    public function description()
+    {
         return "Square has four equal sides.";
     }
 
-    public function getArea($length, $width) {
+    public function getArea($length, $width)
+    {
         if ($length != $width) {
             return "Invalid";
         }
@@ -32,7 +33,8 @@ class Square extends Shape implements IShape {
         return $length * $width;
     }
 
-    public function getPerimeter($length, $width) {
+    public function getPerimeter($length, $width)
+    {
         if ($length != $width) {
             return "Invalid";
         }
@@ -41,44 +43,56 @@ class Square extends Shape implements IShape {
     }
 }
 
-class Rectangle extends Shape implements IShape {
-    public function description() {
+class Rectangle extends Shape implements IShape
+{
+    public function description()
+    {
         return "Rectangle has two equal sides.";
     }
 
-    public function getArea($length, $width) {
+    public function getArea($length, $width)
+    {
         return $length * $width;
     }
 
-    public function getPerimeter($length, $width) {
+    public function getPerimeter($length, $width)
+    {
         return 2 * ($length + $width);
     }
 }
 
-class Triangle extends Shape implements ITriangle {
-    public function description() {
+class Triangle extends Shape implements ITriangle, IShape
+{
+    public function description()
+    {
         return "Triangle has three sides.";
     }
 
-    public function getArea($base, $height) {
+    public function getArea($base, $height)
+    {
         return ($base * $height) / 2;
     }
 
-    public function getPerimeter($side1, $side2, $side3) {
+    public function getPerimeter($side1, $side2, $side3)
+    {
         return $side1 + $side2 + $side3;
     }
 }
 
-class Circle extends Shape implements ICircle {
-    public function description() {
+class Circle extends Shape implements IShape
+{
+    public function description()
+    {
         return "Circle has no sides, only a curve.";
     }
 
-    public function getAreaofCircle($radius) {
+    public function getArea($radius, $unused = null)
+    {
         return round(pi() * pow($radius, 2), 2);
     }
 
-    public function getPerimeter($radius) {
+    public function getPerimeter($radius, $unused = null)
+    {
         return round(2 * pi() * $radius, 3);
     }
 }
@@ -97,10 +111,10 @@ echo $shape2->getPerimeter(4, 6) . "\n"; // output 20
 
 $shape3 = new Triangle();
 echo $shape3->description(); // output "Triangle have three sides."
-echo $shape3->getArea(4,6); // output 12
-echo $shape3->getPerimeter(4,6,7); // output 17
+echo $shape3->getArea(4, 6); // output 12
+echo $shape3->getPerimeter(4, 6, 7); // output 17
 
 $shape4 = new Circle();
 echo $shape4->description(); // output "Circle have no sides only curve."
-echo $shape4->getAreaOfCircle(5); // output 78.54, consider the first parameter as the radius value
+echo $shape4->getArea(5); // output 78.54, consider the first parameter as the radius value
 echo $shape4->getPerimeter(5); // output 31.416, consider the first parameter as the radius value
